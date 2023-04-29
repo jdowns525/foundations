8f52c2bf8d6519077798e6ed1b9ef3dad4c37be0
Josseph Downs <downsjosseph@gmail.com>
2023-04-29 03:58:36 +0000

    foundations models

--- app/models/foundation.rb
+++ app/models/foundation.rb
@@ -23,6 +23,16 @@ class Foundation < ApplicationRecord
   has_many :impacts, dependent: :destroy
 
   include PgSearch::Model
-pg_search_scope :search_by_description, against: :description
+  pg_search_scope :search_by_description, against: :description
+  enum category: { arts: 0, education: 1, health: 2, environment: 3, human_services: 4 }
 
+  STATES = [
+    'Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut',
+    'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa',
+    'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan',
+    'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
+    'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma',
+    'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee',
+    'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
+  ].freeze
 end
