baefa9082a463ded6e78e3b5925eedf8d8d0e615
Josseph Downs <downsjosseph@gmail.com>
2023-04-29 03:12:19 +0000

    events controller methods set

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,47 @@
 class EventsController < ApplicationController
-end
+  before_action :set_foundation
+  before_action :set_event, only: [:edit, :update, :destroy]
+
+  def new
+    @event = @foundation.events.new
+  end
+
+  def create
+    @event = @foundation.events.new(event_params)
+
+    if @event.save
+      redirect_to @foundation, notice: 'Event was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @event.update(event_params)
+      redirect_to @foundation, notice: 'Event was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @event.destroy
+    redirect_to @foundation, notice: 'Event was successfully destroyed.'
+  end
+
+  private
+
+  def set_foundation
+    @foundation = Foundation.find(params[:foundation_id])
+  end
+
+  def set_event
+    @event = @foundation.events.find(params[:id])
+  end
+
+  def event_params
+    params.require(:event).permit(:name, :start_date, :end_date, :location, :description)
+  end
